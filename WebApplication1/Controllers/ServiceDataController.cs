650d07fc590e8921b77d226460fa56ec191613b0
Forgotalready <oleg.burdakin2016@yandex.ru>
2024-11-15 20:18:55 +0700

    3

--- WebApplication1/Controllers/ServiceDataController.cs
+++ WebApplication1/Controllers/ServiceDataController.cs
@@ -4,28 +4,7 @@ using WebApplication1.Service;
 namespace WebApplication1.Controllers;
 
 public class ServiceDataController : Controller
-{
-  private readonly CalculationService _calculationService;
-
-  public ServiceDataController(CalculationService calculationService)
-  {
-    _calculationService = calculationService;
-  }
+{ 
+  public IActionResult Index() => View();
   
-  [NonAction]
-  private void SetData()
-  {
-    var data = _calculationService.Calculate();
-    ViewData["first"] = data["first"];
-    ViewData["second"] = data["second"];
-    ViewData["sum"] = data["sum"];
-    ViewData["mul"] = data["mul"];
-    ViewData["divide"] = data["divide"];
-  }
-
-  public IActionResult Index()
-  {
-    SetData();
-    return View();
-  }
 }
